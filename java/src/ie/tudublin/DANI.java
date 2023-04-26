33b800b683d336176118aab0ab8cee23845dae3a
C2141602 <c21416022@mytudublin.ie>
2023-04-26 16:23:26 +0100

    Make a class called Follow

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -41,4 +41,43 @@ public class DANI extends PApplet {
         textAlign(CENTER, CENTER);
         
 	}
+
+	public class Follow {
+		private String word;
+		private int count;
+	
+		// Constructor with both fields
+		public Follow(String word, int count) {
+			this.word = word;
+			this.count = count;
+		}
+	
+		// Constructor with just the word field
+		public Follow(String word) {
+			this(word, 0);
+		}
+	
+		// Accessor method for the word field
+		public String getWord() {
+			return word;
+		}
+	
+		// Accessor method for the count field
+		public int getCount() {
+			return count;
+		}
+	
+		// Method to increment the count field by one
+		public void incrementCount() {
+			count++;
+		}
+	
+		// toString method to print out the object's fields
+		public String toString() {
+			return word + ": " + count;
+		}
+	}
+	
+
+
 }
